3b300c89285f9f0f9c35ce0bcf73e20a0d8e6074
Danny <danykhosravi@gmail.com>
2024-07-16 15:45:08 +0330

    ready to disbale io reader hash

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module v2p
 go 1.21.4
 
 require (
+	github.com/corona10/goimagehash v1.1.0
 	github.com/disintegration/imaging v1.6.2
 	github.com/u2takey/ffmpeg-go v0.5.0
 )
@@ -10,6 +11,7 @@ require (
 require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
 )
---
+++ hash.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io"
+	"os"
+)
+
+var map_hash = make(map[string]bool)
+var append_hash = []string{}
+var itr int = 0
+var Reset = "\033[0m"
+var Red = "\033[31m"
+
+func insertHash(s string) bool {
+
+	fmt.Println(Red + s + Reset)
+	if map_hash[s] {
+		return false // Already in the map
+	}
+	if itr > 0 {
+		append_hash = append(append_hash, s)
+		map_hash[s] = true
+		itr = 0
+		return true
+	}
+	itr++
+	return false
+}
+
+type HashInfo struct {
+	Md5    string `json:"md5"`
+	Sha1   string `json:"sha1"`
+	Sha256 string `json:"sha256"`
+	Sha512 string `json:"sha512"`
+}
+
+func CalculateBasicHashes(rd io.Reader) HashInfo {
+
+	md5 := md5.New()
+	sha1 := sha1.New()
+	sha256 := sha256.New()
+	sha512 := sha512.New()
+
+	// For optimum speed, Getpagesize returns the underlying system's memory page size.
+	pagesize := os.Getpagesize()
+
+	// wraps the Reader object into a new buffered reader to read the files in chunks
+	// and buffering them for performance.
+	readers := bufio.NewReaderSize(rd, pagesize)
+
+	// creates a multiplexer Writer object that will duplicate all write
+	// operations when copying data from source into all different hashing algorithms
+	// at the same time
+	multiWriter := io.MultiWriter(md5, sha1, sha256, sha512)
+
+	// Using a buffered reader, this will write to the writer multiplexer
+	// so we only traverse through the file once, and can calculate all hashes
+	// in a single byte buffered scan pass.
+	//
+	_, err := io.Copy(multiWriter, readers)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var info HashInfo
+
+	info.Md5 = hex.EncodeToString(md5.Sum(nil))
+	info.Sha1 = hex.EncodeToString(sha1.Sum(nil))
+	info.Sha256 = hex.EncodeToString(sha256.Sum(nil))
+	info.Sha512 = hex.EncodeToString(sha512.Sum(nil))
+
+	return info
+}
+
+// HasherReader calculates the hash of a byte stream
+// As an underlying io.Reader is read from, the hash is updated
+type HasherReader struct {
+	hash   hash.Hash
+	reader io.Reader
+}
+
+// NewHasherReader creates a new HasherReader from a provided io.Raeder
+func NewHasherReader(r io.Reader) HasherReader {
+	hash := sha1.New()
+	reader := io.TeeReader(r, hash)
+	return HasherReader{hash, reader}
+}
+
+// Hash returns the hash value
+// Ensure all contents of the underlying io.Reader have been read
+func (h HasherReader) Hash() string {
+	return hex.EncodeToString(h.hash.Sum(nil))
+}
+
+// Read allows HasherReader to conform to io.Reader interface
+func (h HasherReader) Read(p []byte) (n int, err error) {
+	return h.reader.Read(p)
+}
